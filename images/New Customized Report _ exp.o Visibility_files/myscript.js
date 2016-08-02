c7c6a159d9a9a19a611419cc94a894ec27b49d57
Kelsey Aiello <kelseymaiello@gmail.com>
2016-08-02 16:16:10 -0700

    changes -Kelsey

---
+++ images/New Customized Report _ exp.o Visibility_files/myscript.js
@@ -0,0 +1,9 @@
+'use strict';
+
+document.getElementById("remove").addEventListener("click", function(){
+    document.getElementById("expeditors-reference-button").style.visibility = "hidden";
+});
+
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
